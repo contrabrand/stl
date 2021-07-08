0bd5478163975415261adbd60afdb0b96de00d4d
contrabrand <86887091+contrabrand@users.noreply.github.com>
2021-07-09 01:40:30 +0700

    Add files via upload

---
+++ vpn.php
@@ -0,0 +1,48 @@
+{"Version":"1.0.2",
+
+"Url":"https://github.com/contrabrand/stl/raw/main/vpn.zip" ,
+
+"Changelog":"  
+
+🙏 WELCOME TO Stallion OVPN🙏
+
+            Fast  Secure Stable
+
+Glider OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed. 
+
+What we privode in our app
+
+🟢 AIS 🇹🇭
+🔵 DTAC 🇹🇭
+🔴 TRUE 🇹🇭
+🔴 MAXIS 🇲🇾 🔜
+🟡 DIGI 🇲🇾 🔜
+🟠 Umobile 🇲🇾 🔜
+🔵 Celcom 🇲🇾 🔜
+🔴 Telkomsel 🇲🇨 🔜
+
+Our server location
+🇸🇬
+🇹🇭 
+🇲🇾 🔜
+🇲🇨 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
