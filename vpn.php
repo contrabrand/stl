e6f708bc070a0c220154d907a03954e4f1c0f1bf
contrabrand <86887091+contrabrand@users.noreply.github.com>
2021-07-11 02:08:23 +0700

    Create vpn.php

---
+++ vpn.php
@@ -0,0 +1,60 @@
+{"Version":"2.1",
+
+"Url":"https://github.com/contrabrand/stl/raw/main/vpn.zip" ,
+
+"Changelog":"  
+
+        🙏 WELCOME TO Stallion OVPN🙏
+
+               Fast  Secure Stable
+  
+Stallion OVPN is a free and VIP  app for those who want to access Internet securely and get more faster download speed.
+
+Update on 11 July 2021 
+
+New added files
+
+MAXIS-TH 🇹🇭
+CELCOM-TH 🇹🇭
+DIGI-TH 🇹🇭
+UMOBILE-TH 🇹🇭
+
+* สำหรับ​ไฟล์วีไอพี ติดต่อแอดมินเพื่อขอ User และ​ Password
+* Please contact admin to get username and password
+
+What we privode in our app
+
+🟢 AIS 🇹🇭
+🔵 DTAC 🇹🇭
+🔴 TRUE 🇹🇭 
+🔴 MAXIS 🇲🇾 
+🟡 DIGI 🇲🇾 
+🟠 Umobile 🇲🇾 
+🔵 Celcom 🇲🇾 
+🔴 Telkomsel 🇲🇨 🔜
+
+Our server location
+🇸🇬
+🇹🇭 
+🇲🇾 🔜
+🇲🇨 🔜
+
+Please choose the nearest server to your location for connection stability and get high-speed performance 
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
